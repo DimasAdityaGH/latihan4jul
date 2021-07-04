befdbc5009bf5e89886b033245c8b8e6f29ebb9b
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-07-04 13:29:11 +0700

    menambahkan code dasar

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
-package latihan4jul
+package main
+
+func main() {
+	//hello world
+
+
+
+
+	//string
+
+
+	//const
+
+
+
+	//var
+
+
+	//perbandingan
+
+
+	//operasi matematika
+
+
+
+
+	//type declaration
+
+
+
+	//boolean
+
+
+	//conversion
+
+
+	//type data number
+}
